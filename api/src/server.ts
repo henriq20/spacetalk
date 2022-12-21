3caaf904b25771f9711f31dbde2d338c2a188b42
henriq20 <henrique-ferreira20@hotmail.com>
2022-12-21 09:37:17 -0300

    build: setup tsc-watch to watch files

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,1 +1,8 @@
+import './config.js';
+import express from 'express';
 
+const app = express();
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server listening at port ${ process.env.PORT }`);
+});
